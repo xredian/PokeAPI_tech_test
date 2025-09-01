40598acf685d84062faff4a61c32aa3d7af81d33
xredian <ulyanadyakova@gmail.com>
2025-09-01 13:59:25 +0200

    Added core functions to collect external API data, as well as a function to calculate statistics

---
+++ poke_api.py
@@ -0,0 +1,57 @@
+from fastapi import FastAPI, HTTPException
+from requests import get
+from model import BerryStatistics
+from statistics import mean, median, pvariance
+from dotenv import load_dotenv
+from os import getenv
+load_dotenv()
+
+app = FastAPI(title="Poke-berries statistics API")
+
+pokeapi = getenv("POKEAPI")
+
+
+def get_all_berries() -> list:
+    response = get(f"{pokeapi}?limit=100")
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code,
+                            detail="Can't get berries")
+    return response.json()["results"]
+
+
+def get_berry_by_url(url: str):
+    response = get(url)
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code,
+                            detail=f"Can't get berry number {url.split('/')[-1]}, {url}")
+    return response.json()
+
+
+def calculate_statistics() -> BerryStatistics:
+    berries = get_all_berries()
+    names = [berry["name"] for berry in berries]
+    urls = [berry["url"] for berry in berries]
+
+    berries_data = [get_berry_by_url(url) for url in urls]
+    growth_times = [berry["growth_time"] for berry in berries_data]
+
+    min_gt = min(growth_times)
+    median_gt = median(growth_times)
+    max_gt = max(growth_times)
+    mean_gt = mean(growth_times)
+    variance_gt = pvariance(growth_times, mean_gt)
+
+    frequency_gt = {}
+    for time in growth_times:
+        frequency_gt[time] = frequency_gt.get(time, 0) + 1
+
+    return BerryStatistics(
+        berries_names=names,
+        min_growth_time=min_gt,
+        median_growth_time=median_gt,
+        max_growth_time=max_gt,
+        variance_growth_time=variance_gt,
+        mean_growth_time=mean_gt,
+        frequency_growth_time=frequency_gt
+    )
+
